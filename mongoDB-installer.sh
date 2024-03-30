549fc866bb2941aaa20e194dd61b1a7869fbff02
Cris <c.cheuquel01@ufromail.cl>
2024-03-30 18:39:29 -0300

    indentacion corregida

--- mongoDB-installer.sh
+++ mongoDB-installer.sh
@@ -2,6 +2,37 @@
 
 # Install MongoDB on Ubuntu 22.04
 # Author: Cris
+
+enable_mongodb(){
+	sudo systemctl start mongodb
+	sudo systemctl enable mongodb
+	sudo systemctl status mongodb
+}
+install_mongodb() {
+	sudo apt update > /dev/null
+	sudo apt install -y mongodb-org > /dev/null
+	read -p "Deseas habilitar y arrancar el servicio de MongoDB? (y/n): " response
+	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+		enable_mongodb
+	else
+		echo "No se ha habilitado el servicio de MongoDB"
+	fi
+}
+setup_mongodb(){
+	echo "Instalando los comandos necesarios para importar la clave publica de MongoDB"
+	sudo apt-get install gnupg curl > /dev/null
+	
+	echo "importando la clave publica de MongoDB"
+	curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+	echo "Creando el archivo de lista de repositorios de MongoDB en /etc/apt/sources.list.d/mongodb-org-7.0.list"
+	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+	echo "Actualizando la lista de paquetes"
+	sudo apt-get update > /dev/null
+	echo "Instalando MongoDB"
+	install_mongodb
+}
 run(){
 		if [[ -x "$(command -v lsb_release)" ]]; then
 		ubuntu_version=$(lsb_release -rs)
@@ -19,34 +50,5 @@ run(){
 	fi
 
 }
-install_mongodb() {
-	sudo apt update > /dev/null
-	sudo apt install -y mongodb-org > /dev/null
-	read -p "Deseas habilitar y arrancar el servicio de MongoDB? (y/n): " response
-	if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-		enable_mongodb
-	else
-		echo "No se ha habilitado el servicio de MongoDB"
-	fi
-}
-enable_mongodb(){
-	sudo systemctl start mongodb
-	sudo systemctl enable mongodb
-	sudo systemctl status mongodb
-}
-setup_mongodb(){
-	echo "Instalando los comandos necesarios para importar la clave publica de MongoDB"
-	sudo apt-get install gnupg curl > /dev/null
-	
-	echo "importando la clave publica de MongoDB"
-	curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-	echo "Creando el archivo de lista de repositorios de MongoDB en /etc/apt/sources.list.d/mongodb-org-7.0.list"
-	echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-	echo "Actualizando la lista de paquetes"
-	sudo apt-get update > /dev/null
-	echo "Instalando MongoDB"
-	install_mongodb
-}
+
 run
